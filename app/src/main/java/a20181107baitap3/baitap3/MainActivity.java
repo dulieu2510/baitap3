4585bbba5b68604b465d02cb6da6846c73144993
dulieu2510 <giaptg@gmail.com>
2018-11-07 18:51:10 +0700

    Initial commit

---
+++ app/src/main/java/a20181107baitap3/baitap3/MainActivity.java
@@ -0,0 +1,13 @@
+package a20181107baitap3.baitap3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
